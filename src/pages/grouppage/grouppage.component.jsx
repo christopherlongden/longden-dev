e21fe310440405b13df77514e3b164d3e1755fdc
Chris Longden <longden@longden.net>
2020-03-16 12:54:05 +0100

    refactor newspage to use hooks

--- src/pages/grouppage/grouppage.component.jsx
+++ src/pages/grouppage/grouppage.component.jsx
@@ -1,112 +1,102 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import GroupPageContainer from './grouppage.styles'
 import { firestore, addDocument, convertNewsSnapshotToMap } from '../../firebase/firebase.utils.js';
 import NewsItems from '../../components/news-list/news-list.component';
 import AddNewsItem from '../../components/add-news-item/add-news-item.component';
 
-class GroupPage extends React.Component {
-    unsubscribeFromGroupSnapshot = null;
-    unsubscribeFromNewsSnapshot = null;
+function GroupPage(props) {
+    const [news, setNews] = useState([]);
+    const [group, setGroup] = useState({ name: null, id: props.match.params.id });
+    const [newsItemTitle, setNewsItemTitle] = useState('');
+    const [newsItemBody, setNewsItemBody] = useState('');
+    const [showAddNews, setShowAddNews] = useState(false);
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            news: [],
-            group: {
-                name: null,
-                id: props.match.params.id
-            },
-            newsItemTitle: '',
-            newsItemBody: '',
-            showAddNews: false
-        }
-
-        console.log(this.props);
-    }
-
-    handleSubmit = async event => {
+    async function handleSubmit(event) {
         event.preventDefault();
 
-        const { newsItemTitle, newsItemBody, group } = this.state;
         const newNewsItem = { title: newsItemTitle, body: newsItemBody, groups: [ group.id ], created: new Date() };
         
         await addDocument('news', newNewsItem);
-        this.setState({ newsItemTitle: '', newsItemBody: '', showAddNews: false })
+        setNewsItemTitle('');
+        setNewsItemBody('');
+        setShowAddNews(false);
     }
 
-    isFormValid = () => {
-        const { newsItemTitle, newsItemBody } = this.state;
-        return newsItemTitle && newsItemBody;
+    function isFormValid() {
+        return newsItemTitle.length > 10 && newsItemBody.length > 10;
     }
 
-    handleChange = event => {
+    function handleChange(event) {
         const { name, value } = event.target;
-        this.setState({ [name]: value });
+        if (name === 'newsItemTitle') {
+            setNewsItemTitle(value);
+        }
+        if (name === 'newsItemBody') {
+            setNewsItemBody(value);
+        }
     };
 
-    toggleNewPostVisbility = () => {
-        this.setState( { showAddNews: !this.state.showAddNews } )
+    function toggleNewPostVisbility() {
+        setShowAddNews(!showAddNews);
     }
-    
-    componentDidMount() {
-        const groupId = this.state.group.id;
-        const groupRef = firestore.collection('group').doc(groupId);
-        this.unsubscribeFromGroupSnapshot = groupRef.onSnapshot(snapshot => {
-            const group = { id: this.state.group.id, ...snapshot.data() }
-            this.setState ( { 
-                group: group
-            });
+
+    useEffect(() => {
+        const groupRef = firestore.collection('group').doc(group.id);
+        const unsubscribeFromGroupSnapshot = groupRef.onSnapshot(snapshot => {
+            const thisGroup = { id: group.id, ...snapshot.data() }
+            setGroup(thisGroup);
         });
 
         // todo: https://firebase.google.com/docs/firestore/query-data/query-cursors
-        const newsRef = firestore.collection("news").where("groups", "array-contains", groupId).orderBy("created", "desc").limit(5);
-        this.unsubscribeFromNewsSnapshot = newsRef.onSnapshot(snapshot => {
-            this.setState ( { 
-                news: convertNewsSnapshotToMap(snapshot) 
-            });
+        const newsRef = firestore.collection("news").where("groups", "array-contains", group.id).orderBy("created", "desc").limit(5);
+        const unsubscribeFromNewsSnapshot = newsRef.onSnapshot(snapshot => {
+            setNews(convertNewsSnapshotToMap(snapshot));
         });
-    }
-
-    componentWillUnmount() {
-        this.unsubscribeFromGroupSnapshot();
-        this.unsubscribeFromNewsSnapshot();
-    }
-
-    render() {
-        const { newsItemTitle, newsItemBody, group, news, showAddNews } = this.state;
-        return (
-            <GroupPageContainer>
-                <h3>Group Page: { group.name }</h3>
-                
-                {/* <h4>Members</h4>
-
-                <h4>Chat</h4> */}
     
-                <h4>News</h4>
+        return function cleanup() {
+            unsubscribeFromGroupSnapshot();
+            unsubscribeFromNewsSnapshot();
+        }
+    });
 
-                <NewsItems news={news}/>
 
+    return (
+        <GroupPageContainer>
+            <h3>Group Page: { group.name }</h3>
+            
+            {/* <h4>Members</h4>
+
+            <h4>Chat</h4> */}
+
+            <h4>News</h4>
+
+            <NewsItems news={news}/>
+
+            { props.currentUser ?
                 <div className="toggle-box">
-                    <input type="button" onClick={this.toggleNewPostVisbility} value="Add New Post"/>
+                    <input type="button" onClick={toggleNewPostVisbility} value="Add New Post"/>
                 </div>
-                { showAddNews ?
-                    <div>
-                        <h3>New News Post</h3>
-                        <AddNewsItem
-                            title={newsItemTitle}
-                            body={newsItemBody}
-                            group={group} 
-                            isFormValid={this.isFormValid}
-                            handleSubmit={this.handleSubmit} 
-                            handleChange={this.handleChange} 
-                        />
-                    </div>
-                    :
-                    null }
-            </GroupPageContainer>
-        )
-    }
+                :
+                null
+            }
+
+            { showAddNews ?
+                <div>
+                    <h3>New News Post</h3>
+                    <AddNewsItem
+                        title={newsItemTitle}
+                        body={newsItemBody}
+                        group={group} 
+                        isFormValid={isFormValid}
+                        handleSubmit={handleSubmit} 
+                        handleChange={handleChange} 
+                    />
+                </div>
+                :
+                null
+            }
+        </GroupPageContainer>
+    )
 }
 
 export default GroupPage;
